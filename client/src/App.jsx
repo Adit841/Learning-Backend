31ef54a73650dd8e89054dca36aef37649e6217b
Adit841 <adityagour0841@gmail.com>
2026-06-30 12:12:34 +0530

    feed done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import CreatePost from "./pages/CreatePost";
+import Feed from "./pages/Feed"
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/create-post" element={<CreatePost/>}></Route>
-        <Route path="/about" element={<h1>Hello nice about </h1>}></Route>
+        <Route path="/create-post" element={<CreatePost/>} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
   );
